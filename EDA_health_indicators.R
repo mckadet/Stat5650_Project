4f46c9fd34e05fdaf11f3d6b1e7bcdba0cd20c81
McKade Thomas <mckadet@gmail.com>
2022-03-23 15:18:21 -0600

    Initial script for exploring the data

---
+++ EDA_health_indicators.R
@@ -0,0 +1,11 @@
+## Stat5650 Group Project
+## Group Members: 
+# - Nate Nellis
+# - Brian Nalley
+# - Will Guillon
+# - McKade Thomas
+
+## Libraries Needed
+
+## Read in the data
+health <- read.csv("heart_disease_health_indicators.csv")
