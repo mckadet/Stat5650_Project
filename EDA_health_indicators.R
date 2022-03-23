1cc7283aa02bd95d46eadc7e428c903dd8c1c559
McKade Thomas <mckadet@gmail.com>
2022-03-23 15:30:02 -0600

    Adding descriptions

--- EDA_health_indicators.R
+++ EDA_health_indicators.R
@@ -9,3 +9,31 @@
 
 ## Read in the data
 health <- read.csv("heart_disease_health_indicators.csv")
+
+## Response
+# - HeartDiseaseorAttack, binary
+
+## Predictor Variables
+## 14 factors, 6 numeric, 1 categorical
+# HighBP, factor
+# HighChol, factor
+# CholCheck, factor
+# BMI, num
+# Smoker, factor
+# Stroke, factor
+# Diabetes, factor
+# PhysActivity, factor
+# Fruits, factor
+# Veggies, factor
+# HvyAlcoholConsum, factor
+# AnyHealthcare, factor
+# NoDocbcCost, factor
+# GenHlth, num
+# MentHlth, num
+# PhysHlth, num
+# DiffWalk, factor
+# Sex, factor
+# Age, num
+# Education, categorical
+# Income, num
+
